f1d30a0ac8279d97b8c256d1ade049c0c3981a52
nam-web <65065658+nam-web@users.noreply.github.com>
2024-06-01 16:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function showAlert() {
+            // Get the form values
+            var firstName = document.getElementById('firstName').value;
+            var lastName = document.getElementById('lastName').value;
+            var phoneNumber = document.getElementById('phoneNumber').value;
+            var emailId = document.getElementById('emailId').value;
+            
+            // Construct the alert message
+            var message = 'First Name: ' + firstName + ' ' +
+                          'Last Name: ' + lastName + ' ' +
+                          'Phone Number: ' + phoneNumber + ' ' +
+                          'Email ID: ' + emailId;
+
+            // Show the alert
+            alert(message);
+
+            // Prevent form submission
+            return false;
+        }
